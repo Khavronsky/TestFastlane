0dc59207b1f4ce7fb56f693d37e27a9fe5820bcb
khavronsky <khavronsky@gmail.com>
2017-12-19 10:09:24 +0300

    Initial commit

---
+++ app/src/main/java/com/khavronsky/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.khavronsky.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.tv);
+        mTextView.setText("test_text");
+    }
+}
